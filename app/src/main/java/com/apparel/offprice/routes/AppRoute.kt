3692df1177313197a4755673437a6e84ef803de1
Saqib Ahmed <saqibahmed@colanonline.com>
2025-12-29 19:31:33 +0530

    resolved the merged conflicts

--- app/src/main/java/com/apparel/offprice/routes/AppRoute.kt
+++ app/src/main/java/com/apparel/offprice/routes/AppRoute.kt
@@ -1,6 +1,5 @@
 package com.apparel.offprice.routes
 
-import BottomNavScreen
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
@@ -16,7 +15,6 @@ import com.apparel.offprice.features.home.presentation.screens.home.HomeScreen
 import com.apparel.offprice.features.home.presentation.screens.search.SearchScreen
 import com.apparel.offprice.features.paymentCard.presentation.screen.PaymentCardScreen
 import com.apparel.offprice.features.pdp.presentation.screen.PDPScreen
-import com.apparel.offprice.features.pdp.presentation.screen.PDPscreen
 import com.apparel.offprice.features.plp.presentation.screens.PLPScreen
 import com.apparel.offprice.features.profile.presentation.screen.profileDetails.ProfileDetailsScreen
 import com.apparel.offprice.features.profile.presentation.screen.profilePassword.ProfilePasswordScreen
@@ -75,7 +73,7 @@ fun AppRoutes(windowSizeClass: WindowSizeClass) {
         }
 
         composable<AppScreen.HomeScreen> {
-            HomeScreen (onNavigateToOuter = { route ->
+            HomeScreen(onNavigateToOuter = { route ->
                 navController.navigate(route)
             })
         }
@@ -114,7 +112,6 @@ fun AppRoutes(windowSizeClass: WindowSizeClass) {
             )
         }
         composable<AppScreen.SubCategoryScreen> { backStackEntry ->
-
             val args = backStackEntry.toRoute<AppScreen.SubCategoryScreen>()
             SubCategoryScreen(
                 title = args.title,
@@ -160,8 +157,8 @@ fun AppRoutes(windowSizeClass: WindowSizeClass) {
         }
 
         composable<AppScreen.PDPScreen> { backStack ->
-            val productId = backStack.arguments?.getString("id")
-            PDPScreen(productId)
+            val args = backStack.toRoute<AppScreen.PDPScreen>()
+            PDPScreen(args.id)
         }
 
         composable<AppScreen.UserProfileScreen> {
